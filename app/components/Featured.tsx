bddc3b2a5a565be912a820a162c56c70e8e0ad73
Usman <130107471+UsmanFaisal13@users.noreply.github.com>
2025-06-20 15:56:14 -0700

    Update Featured.tsx

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const Featured = () => {
     return (
         <div className='flex flex-col gap-6 lg:px-44'>
-            <h1 className='font-heebo text-[22px] font-medium leading-[32.31px]'>Featured works</h1>
+            <h1 className='font-heebo text-[22px] font-medium leading-[32.31px] pl-2 lg:pl-0'>Featured works</h1>
             <div className='flex flex-col lg:gap-32 gap-64'>
                 <Card src='/Rectangle 30.png' />
                 <Card src='/Rectangle 32.png' />
